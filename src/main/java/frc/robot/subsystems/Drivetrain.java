0ccd9cb3d68c18caff784825d4a1af6e0e80d2ce
Kobi Jones <kobijonesmail@gmail.com>
2019-10-09 17:24:26 -0500

    Minor changes

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -15,39 +15,37 @@ import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.Constants;
-import frc.robot.OI;
-import frc.robot.Robot;
 import frc.robot.commands.ArcadeDrive;
 
 /**
  * Add your docs here.
  */
 public class Drivetrain extends Subsystem {
-    private static int leftFrontMotorID = 1;
-    private static int leftRearMotorID = 2;
-    private static int leftEncoderChannelA = 0;
-    private static int leftEncoderChannelB = 1;
+    private static final int leftFrontMotorID = 1;
+    private static final int leftRearMotorID = 2;
+    private static final int rightFrontMotorID = 3;
+    private static final int rightRearMotorID = 4;
 
-    private static int rightFrontMotorID = 3;
-    private static int rightRearMotorID = 4;
-    private static int rightEncoderChannelA = 2;
-    private static int rightEncoderChannelB = 3;
+    private static final int leftEncoderChannelA = 0;
+    private static final int leftEncoderChannelB = 1;
+    private static final int rightEncoderChannelA = 2;
+    private static final int rightEncoderChannelB = 3;
 
-    private static double gearRatio = 1; //Don't know this atm
-    private static double encoderPulsesPerRevolution = 128 * gearRatio; //Pulses per revolution of whatever encoders we use
-    private static double encoderRevolutionsPerPulse = 1 / encoderPulsesPerRevolution;
-    private static double wheelCircumference = Math.PI * Constants.WHEEL_DIAMETER;
-    private static double encoderDistancePerPulse = encoderRevolutionsPerPulse * wheelCircumference;
+    private static final double gearRatio = 1; //Don't know this atm
+    private static final double encoderPulsesPerRevolution = 128 * gearRatio; //Pulses per revolution of whatever encoders we use
+    private static final double encoderRevolutionsPerPulse = 1 / encoderPulsesPerRevolution;
+    private static final double wheelCircumference = Math.PI * Constants.WHEEL_DIAMETER;
+    private static final double encoderDistancePerPulse = encoderRevolutionsPerPulse * wheelCircumference;
 
-    private WPI_TalonSRX leftFrontMotor;
-    private WPI_TalonSRX leftRearMotor;
-    private WPI_TalonSRX rightFrontMotor;
-    private WPI_TalonSRX rightRearMotor;
-    private static SpeedControllerGroup leftGroup;
-    private static SpeedControllerGroup rightGroup;
-    private static DifferentialDrive drivetrain;
-    private Encoder leftEncoder;
-    private Encoder rightEncoder;
+    private final WPI_TalonSRX leftFrontMotor;
+    private final WPI_TalonSRX leftRearMotor;
+    private final WPI_TalonSRX rightFrontMotor;
+    private final WPI_TalonSRX rightRearMotor;
+    private final SpeedControllerGroup leftGroup;
+    private final SpeedControllerGroup rightGroup;
+    private final DifferentialDrive drivetrain;
+    private final Encoder leftEncoder;
+    private final Encoder rightEncoder;
 
     public Drivetrain() {
         leftFrontMotor = new WPI_TalonSRX(leftFrontMotorID);
