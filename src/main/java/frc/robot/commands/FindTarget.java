d803908ec009490877216df06ef771e3538529fe
ike <ihschw241@isd709.org>
2019-11-13 17:11:58 -0600

    finished merge

--- src/main/java/frc/robot/commands/FindTarget.java
+++ src/main/java/frc/robot/commands/FindTarget.java
@@ -6,15 +6,9 @@ import frc.robot.Robot;
 
 public class FindTarget extends Command {
 
-<<<<<<< HEAD
-    private final double desiredTa = 5.0; //the accepted final range of degrees of the target at the end of the maneuver.
-    final double DRIVE_K = -0.3;                    // how hard to drive fwd toward the target
-    final double STEER_K = -0.03;                    // how hard to turn toward the target
-=======
     private final double desiredTa = 9.0; //the accepted final range of degrees of the target at the end of the maneuver.
     final double DRIVE_K = 0.26;                    // how hard to drive fwd toward the target
     final double STEER_K = 0.02;                    // how hard to turn toward the target
->>>>>>> 5278b0f090c17fb27553401b39bd0ed79599e4c2
 
 
     private double turnSpeed = 0.05;
@@ -32,24 +26,14 @@ public class FindTarget extends Command {
 
     @Override
     protected void execute() {
-<<<<<<< HEAD
-        turnSpeed = 0.5;
-        driveSpeed = 0.0;
-=======
->>>>>>> 5278b0f090c17fb27553401b39bd0ed79599e4c2
         if (Robot.m_limelight.getTargetValid()) {
             //true when the crosshairs are outside of the tolerance
             //gets the limelight data
           //  if (Math.abs(headingError) > tolerance) {
                 //calculates the ramped speed between the minimum speed and one using the ramp coefficient
                 //turnSpeed = Math.min(Math.max(headingError * rampCoeff, minSpeed), 1); 
-<<<<<<< HEAD
-                turnSpeed = Robot.m_limelight.getHorizontalOffset() * STEER_K;
-                driveSpeed = (desiredTa - Robot.m_limelight.getTargetArea()) * DRIVE_K;
-=======
                 turnSpeed = -(Robot.m_limelight.getHorizontalOffset() * STEER_K);
                 driveSpeed = Math.min((desiredTa - Robot.m_limelight.getTargetArea()) * DRIVE_K, 0.7);
->>>>>>> 5278b0f090c17fb27553401b39bd0ed79599e4c2
         //    }
         } else {
             turnSpeed = 0.5;
