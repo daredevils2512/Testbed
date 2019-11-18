379a5332c51f810b35de52b7542b4757540e9ba8
ike <ihschw241@isd709.org>
2019-11-18 16:51:55 -0600

    ball following

--- src/main/java/frc/robot/commands/FindTarget.java
+++ src/main/java/frc/robot/commands/FindTarget.java
@@ -6,8 +6,8 @@ import frc.robot.Robot;
 
 public class FindTarget extends Command {
 
-    private final double desiredTa = 9.0; //the accepted final range of degrees of the target at the end of the maneuver.
-    final double DRIVE_K = 0.26;                    // how hard to drive fwd toward the target
+    private final double desiredTa = 30.0; //the accepted final range of degrees of the target at the end of the maneuver.
+    final double DRIVE_K = 0.22;                    // how hard to drive fwd toward the target
     final double STEER_K = 0.02;                    // how hard to turn toward the target
 
 
@@ -36,7 +36,6 @@ public class FindTarget extends Command {
                 driveSpeed = Math.min((desiredTa - Robot.m_limelight.getTargetArea()) * DRIVE_K, 0.7);
         //    }
         } else {
-            turnSpeed = 0.5;
             driveSpeed = 0.1;
         }
         SmartDashboard.putNumber("turn speed", turnSpeed);
